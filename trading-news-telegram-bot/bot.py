5d6166f0604cf3a9f831c0ab631984818fe768c0
Christopher Goh <chrisgzf@gmail.com>
2021-01-24 03:13:56 +0800

    Run black

--- trading-news-telegram-bot/bot.py
+++ trading-news-telegram-bot/bot.py
@@ -25,39 +25,46 @@ except ImportError:
 
 poll_delay = 30
 
-twitter_consumer_key = os.environ.get(
-    "TWITTER_CONSUMER_KEY") or config.twitter_consumer_key
-twitter_consumer_secret = os.environ.get(
-    "TWITTER_CONSUMER_SECRET") or config.twitter_consumer_secret
-twitter_access_token = os.environ.get(
-    "TWITTER_ACCESS_TOKEN") or config.twitter_access_token
-twitter_access_token_secret = os.environ.get(
-    "TWITTER_ACCESS_TOKEN_SECRET") or config.twitter_access_token_secret
-twitter_subscribed_list_id = os.environ.get(
-    "TWITTER_SUBSCRIBED_LIST_ID") or config.twitter_subscribed_list_id
+twitter_consumer_key = (
+    os.environ.get("TWITTER_CONSUMER_KEY") or config.twitter_consumer_key
+)
+twitter_consumer_secret = (
+    os.environ.get("TWITTER_CONSUMER_SECRET") or config.twitter_consumer_secret
+)
+twitter_access_token = (
+    os.environ.get("TWITTER_ACCESS_TOKEN") or config.twitter_access_token
+)
+twitter_access_token_secret = (
+    os.environ.get("TWITTER_ACCESS_TOKEN_SECRET") or config.twitter_access_token_secret
+)
+twitter_subscribed_list_id = (
+    os.environ.get("TWITTER_SUBSCRIBED_LIST_ID") or config.twitter_subscribed_list_id
+)
 
 telegram_token = os.environ.get("TELEGRAM_TOKEN") or config.telegram_token
-telegram_group_id = os.environ.get(
-    "TELEGRAM_GROUP_ID") or config.telegram_group_id
+telegram_group_id = os.environ.get("TELEGRAM_GROUP_ID") or config.telegram_group_id
 
-matplotlib.use('agg')
+matplotlib.use("agg")
 
-def send_graph_using_ticker(update: Update,
-                            context: CallbackContext,
-                            t,
-                            graph_period: str,
-                            ticker: str,
-                            interval: str = "1m",
-                            prepost: bool = False):
-    history = t.history(period=graph_period,
-                        interval=interval,
-                        prepost=prepost)
+
+def send_graph_using_ticker(
+    update: Update,
+    context: CallbackContext,
+    t,
+    graph_period: str,
+    ticker: str,
+    interval: str = "1m",
+    prepost: bool = False,
+):
+    history = t.history(period=graph_period, interval=interval, prepost=prepost)
     last_data = history.tail(1)
     try:
         last_data_time = last_data.index.strftime("%H:%M:%S")[0]
     except AttributeError:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Stock info not found. Did you type the right symbol?")
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Stock info not found. Did you type the right symbol?",
+        )
         return
 
     last_data_price = round(last_data["Open"][0], 2)
@@ -68,8 +75,8 @@ def send_graph_using_ticker(update: Update,
     ind = np.arange(N)
 
     def format_date(x, pos=None):
-        thisind = np.clip(int(x+0.5), 0, N-1)
-        return dt[thisind].strftime('%Y-%m-%d')
+        thisind = np.clip(int(x + 0.5), 0, N - 1)
+        return dt[thisind].strftime("%Y-%m-%d")
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -84,8 +91,7 @@ def send_graph_using_ticker(update: Update,
     img.seek(0)
     fig.clf()
 
-    context.bot.send_photo(chat_id=update.effective_chat.id,
-                           photo=img)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=img)
 
 
 def s(update: Update, context: CallbackContext):
@@ -103,8 +109,10 @@ def search(update: Update, context: CallbackContext, send_graph: bool) -> None:
     try:
         info = t.info
     except KeyError:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Stock info not found. Did you type the right symbol?")
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Stock info not found. Did you type the right symbol?",
+        )
         return
 
     stock_name = info.get("longName") or info.get("shortName") or info.get("longName")
@@ -112,10 +120,12 @@ def search(update: Update, context: CallbackContext, send_graph: bool) -> None:
 Day Low/High: {escape_markdown(str(info["dayLow"]), version=2)} {escape_markdown(str(info["dayHigh"]), version=2)}
 Bid/Ask: {escape_markdown(str(info["bid"]), version=2)} {escape_markdown(str(info["ask"]), version=2)}"""
 
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=reply,
-                             disable_web_page_preview=True,
-                             parse_mode=ParseMode.MARKDOWN_V2)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=reply,
+        disable_web_page_preview=True,
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
 
     if send_graph:
         send_graph_using_ticker(update, context, t, "1d", ticker)
@@ -125,12 +135,16 @@ def chart(graph_period: str, interval: str, prepost: bool):
     return lambda u, c: send_chart(u, c, graph_period, interval, prepost)
 
 
-def send_chart(update: Update, context: CallbackContext, graph_period: str,
-               interval: str, prepost: bool) -> None:
+def send_chart(
+    update: Update,
+    context: CallbackContext,
+    graph_period: str,
+    interval: str,
+    prepost: bool,
+) -> None:
     ticker = context.args[0]
     t = yf.Ticker(ticker.upper())
-    send_graph_using_ticker(update, context, t, graph_period, ticker, interval,
-                            prepost)
+    send_graph_using_ticker(update, context, t, graph_period, ticker, interval, prepost)
 
 
 def send_tweet_to_telegram(tweet):
@@ -139,7 +153,8 @@ def send_tweet_to_telegram(tweet):
     permalink = escape_markdown(
         f"https://twitter.com/{username}/status/{tweet.id}",
         version=2,
-        entity_type="TEXT_LINKS")
+        entity_type="TEXT_LINKS",
+    )
     body = escape_markdown(tweet.text, version=2)
 
     message = f"""
@@ -170,8 +185,9 @@ last_tweet_id = fin_list[0].id
 
 def poll_list():
     global last_tweet_id
-    fin_list = api.list_timeline(list_id=twitter_subscribed_list_id,
-                                 since_id=last_tweet_id)
+    fin_list = api.list_timeline(
+        list_id=twitter_subscribed_list_id, since_id=last_tweet_id
+    )
     if fin_list:
         for tweet in reversed(fin_list):
             send_tweet_to_telegram(tweet)
@@ -179,11 +195,11 @@ def poll_list():
 
 
 updater = Updater(telegram_token)
-updater.dispatcher.add_handler(CommandHandler('s', s))
-updater.dispatcher.add_handler(CommandHandler('ss', ss))
-updater.dispatcher.add_handler(CommandHandler('1d', chart("1d", "1m", False)))
-updater.dispatcher.add_handler(CommandHandler('3d', chart("3d", "2m", True)))
-updater.dispatcher.add_handler(CommandHandler('5d', chart("5d", "5m", True)))
+updater.dispatcher.add_handler(CommandHandler("s", s))
+updater.dispatcher.add_handler(CommandHandler("ss", ss))
+updater.dispatcher.add_handler(CommandHandler("1d", chart("1d", "1m", False)))
+updater.dispatcher.add_handler(CommandHandler("3d", chart("3d", "2m", True)))
+updater.dispatcher.add_handler(CommandHandler("5d", chart("5d", "5m", True)))
 updater.start_polling()
 
 print("Setup complete. Polling.")
